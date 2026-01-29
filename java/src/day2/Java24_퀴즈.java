b25002ec8f0d88655b29ad38b8d683f3010d0b26
TJ-BU-708-P07 <TJ-BU-708-P07@DESKTOP-OJH1B1R>
2026-01-29 18:24:34 +0900

    260129 반복문 , 조건문 24퀴즈

---
+++ java/src/day2/Java24_퀴즈.java
@@ -0,0 +1,27 @@
+package day2;
+
+import java.util.Scanner;
+
+public class Java24_퀴즈 {
+
+	public static void main(String[] args) {
+		// 1. 사용자가 입력한 숫자의 구구단을 출력하시오.
+		// 2. 2~9사이의 값이 아니면 다시 입력하도록!
+		
+		Scanner s = new Scanner(System.in);
+		for(;;) {
+			System.out.println("구구단 단수를 입력하세요 : ");
+			int num = s.nextInt();
+			
+			if(num >= 2 && num <= 9) {
+				System.out.println("===" + num + "단 ===");
+				for(int i=1; i<=9; i++) {
+					System.out.println(num + " * " + i + " = " + (num*i));
+				}
+				break;
+			} else {
+				System.out.println("2~9 사이의 값을 입력해주세요.");
+			}
+		}System.out.println("프로그램 종료.");
+	}
+}	
